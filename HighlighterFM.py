22bc42a9b8426872807536334f93bbe87f381209
Vini Bueno <>
2021-06-15 16:24:19 -0300

    added percentage informations

--- HighlighterFM.py
+++ HighlighterFM.py
@@ -16,6 +16,12 @@ class HighlighterFM:
     scrobbles_last: int
     average_daily_last: int
 
+    percentage_artists: int
+    percentage_albums: int
+    percentage_tracks: int
+    percentage_scrobbles: int
+    percentage_average_daily: int
+
     # state: str
 
 
@@ -32,11 +38,35 @@ class HighlighterFM:
         object.__setattr__(self, 'scrobbles_last', self.__total(df_tracks_last))
         object.__setattr__(self, 'average_daily_last', round(self.scrobbles_last / 7))
 
+        object.__setattr__(self, 'percentage_artists', self.__percentage(self.artists_cur,self.artists_last))        
+        object.__setattr__(self, 'percentage_albums', self.__percentage(self.albums_cur,self.albums_last))
+        object.__setattr__(self, 'percentage_tracks', self.__percentage(self.tracks_cur,self.tracks_last))
+        object.__setattr__(self, 'percentage_scrobbles', self.__percentage(self.scrobbles_cur, self.scrobbles_last))
+        object.__setattr__(self, 'percentage_average_daily', self.__percentage(self.average_daily_cur, self.average_daily_last))
+
         # object.__setattr__(self, 'state', state)
 
     
     def __str__(self) -> str:
-        return f"Current Artists Listened: {self.artists_cur},\nCurrent Albums Listened: {self.albums_cur},\nCurrent Tracks Listened: {self.tracks_cur},\nCurrent Scrobbles: {self.scrobbles_cur},\nCurrent Average Daily: {self.average_daily_cur}\n\nLast Artists Listened: {self.artists_last},\nLast Albums Listened: {self.albums_last},\nLast Tracks Listened: {self.tracks_last},\nLast Scrobbles: {self.scrobbles_last},\nLast Average Daily: {self.average_daily_last}\n"
+        return f"""
+        Current Artists listened: {self.artists_cur},
+        Current Albums listened: {self.albums_cur},
+        Current Tracks listened: {self.tracks_cur},
+        Current Scrobbles: {self.scrobbles_cur},
+        Current Average Daily: {self.average_daily_cur}
+        
+        Last time Artists listened: {self.artists_last},
+        Last time Albums listened: {self.albums_last},
+        Last time Tracks listened: {self.tracks_last},
+        Last time Scrobbles: {self.scrobbles_last},
+        Last time Average Daily: {self.average_daily_last}
+        
+        Total Artists compared to last time: {self.percentage_artists}%,
+        Total Albums compared to last time: {self.percentage_albums}%,
+        Total Tracks compared to last time: {self.percentage_tracks}%,
+        Total Scrobbles compared to last time: {self.percentage_scrobbles}%,
+        Average Daily compared to last time: {self.percentage_average_daily}%,
+        """
 
 
     @staticmethod
@@ -47,3 +77,8 @@ class HighlighterFM:
     @staticmethod
     def __total(df: pd.DataFrame) -> int:
         return df.index.values.sum()
+
+
+    @staticmethod
+    def __percentage(current: int, last: int) -> int:
+        return round( ((current / last) - 1) * 100 )
